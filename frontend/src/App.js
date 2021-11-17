b9007ed125c9629b622dc3b178507f91f9fe84e8
Davi Mateus Gomes de Almeida <66326378+davimateus1@users.noreply.github.com>
2021-11-17 09:59:36 -0300

    Tabela criada

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,59 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Table, Container, Modalbody, Modal, ModalHeader, FormGroup, ModalFooter } from 'reactstrap';
 
-const App = () => {
+const database = [
+  {matricula: 1, nome: "Davi", cpf: "11145687722", avaliacao: 9},
+  {matricula: 2, nome: "Michael", cpf: "14545687722", avaliacao: 10},
+  {matricula: 3, nome: "João", cpf: "11145658722", avaliacao: 3},
+  {matricula: 4, nome: "Rafael", cpf: "11145907722", avaliacao: 2},
+  {matricula: 5, nome: "Gomes", cpf: "11145652722", avaliacao: 8}
+];
+
+class App extends React.Component{
+
+  state = {
+    database: database
+  }
+  
+  render(){
   return (
-   <h1> Começando </h1>
+    <>
+  <Container>
+    <Button color="primary"> Novo </Button>
+    <h4 class="mt-4 mb-5">Alunos cadastrados</h4>
+    <Table>
+      <thead>
+        <tr>
+          <th>Matricula</th>
+          <th>Nome</th>
+          <th>CPF</th>
+          <th>Avaliação</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          this.state.database.map((item) => (
+            <tr>
+              <td>{item.matricula}</td>
+              <td>{item.nome}</td>
+              <td>{item.cpf}</td>
+              <td>{item.avaliacao}</td>
+              <td>
+              <Button color="success"> Alterar </Button>
+              <Button color="danger"> Remover </Button>
+              </td>
+            </tr>
+          ))
+        }
+      </tbody>
+    </Table>
+  </Container>
+   </>
   );
+  }
 }
 
 export default App;
